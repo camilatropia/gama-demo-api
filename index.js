df4412547beec766bb94fd7bcafb565b39db4d59
douglasmaiabr <douglasmaiabr@gmail.com>
2017-04-08 15:02:40 -0300

    teste de api com node

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
+
+var app = express()
+
+app.use(bodyParser.json())
+
+MongoClient.connect('mongodb://demoUser:gama@ds155490.mlab.com:55490/demo', function (err, db) {
+  if (err) {
+    throw err;
+  }
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+  require('./routes/user.route') (app, _db)
+
+
+  console.log('banco de dados conectado')
+});
